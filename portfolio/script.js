697e1831640f590ccb156db3d14dd57095cc7995
Janki Singh <121485112+janki-singh@users.noreply.github.com>
2024-06-30 18:32:49 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Form submitted! We will get back to you soon.');
+});
